90460c048dd033b2483088d3dd998e8268b090a0
Annie <joshua.velazque@alumnos.udg.mx>
2021-01-12 22:30:34 -0600

    commit inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'lista-empleados',
+    pathMatch: 'full'
+  },
+  {
+    path: 'lista-empleados',
+    component: ListaEmpleadosComponent
+  },
+  {
+    path: 'crear-empleado',
+    component: CrearEmpleadoComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'lista-empleados',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { CrearEmpleadoComponent } from './components/crear-empleado/crear-empleado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListaEmpleadosComponent,
+    CrearEmpleadoComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
